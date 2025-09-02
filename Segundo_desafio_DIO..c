b0b8f28c2845940e1eb89a1f21a48a9c9ee927c8
Ppaulofernando <paulo.paulpoor@gmail.com>
2025-09-02 03:26:07 -0300

    Adicionando arquivos no repositório

---
+++ Segundo_desafio_DIO..c
@@ -0,0 +1,45 @@
+#include <stdio.h> //biblioteca
+#include <conio.h> //biblioteca
+#include <locale.h> //tratamento de String
+#include <math.h> //biblioteca matemática
+#include <string.h>
+#include <windows.h> //biblioteca de operações com String
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+	setlocale(LC_ALL,"Portuguese");
+	system ("color 3E");
+
+	int resultado;
+	char nivel;
+	
+	resultado = rank();	
+	if(resultado<10)                      printf("O herói tem saldo inferior a 10 vitorias e está no nível FERRO.");
+	if(resultado>10  && resultado<=20)    printf("O herói tem saldo %d vitorias e está no nível BRONZE;", resultado);
+	if(resultado>20  && resultado<=50)    printf("O herói tem saldo %d vitorias e está no nível PRATA.", resultado);
+	if(resultado>50  && resultado<=80)    printf("O herói tem saldo %d vitorias e está no nível OURO.", resultado);
+	if(resultado>80  && resultado<=90)    printf("O herói tem saldo %d vitorias e está no nível DIAMANTE.", resultado);
+	if(resultado>90  && resultado<=100)   printf("O herói tem saldo %d vitorias e está no nível LENDÁRIO.", resultado);
+	if(resultado>100)                     printf("O herói tem saldo %d vitorias e está no nível IMORTAL.", resultado);
+	
+	return 0;
+	
+	}
+
+	int rank(int vit, int der) {
+
+		srand(time(NULL) );//retira o valor do relogio de tempo real
+		vit = rand()%100 + 1;
+		der = rand()%100;
+
+		int scor;
+		scor = vit - der;
+
+		return(scor);
+	}
+
+
+
+
